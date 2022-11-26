d8f796a542016636b4c67053ed604307f9d18b46
James Clark <james@citcs.org>
2022-11-26 03:50:35 +0000

    Added rest of flag bitfields boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -77,56 +77,56 @@ uint16_t myuint16 = 0;
 
 
 #define GETBIT00 ( flags & 0b0000000000000001 )
-#define GETBIT01 ( flags & 0b0000000000000010 ) >> 1
-#define GETBIT02 ( flags & 0b0000000000000100 ) >> 2
-#define GETBIT03 ( flags & 0b0000000000001000 ) >> 4
-#define GETBIT04 ( flags & 0b0000000000010000 ) >> 5
-#define GETBIT05 ( flags & 0b0000000000100000 ) >> 6
-#define GETBIT06 ( flags & 0b0000000001000000 ) >> 7
-#define GETBIT07 ( flags & 0b0000000010000000 ) >> 8
-#define GETBIT08 ( flags & 0b0000000100000000 ) >> 9
-#define GETBIT09 ( flags & 0b0000001000000000 ) >> 10
-#define GETBIT10 ( flags & 0b0000010000000000 ) >> 11
-#define GETBIT11 ( flags & 0b0000100000000000 ) >> 12
-#define GETBIT12 ( flags & 0b0001000000000000 ) >> 13
-#define GETBIT13 ( flags & 0b0010000000000000 ) >> 14
-#define GETBIT14 ( flags & 0b0100000000000000 ) >> 15
-#define GETBIT15 ( flags & 0b1000000000000000 ) >> 16
+#define GETBIT01 ( ( flags & 0b0000000000000010 ) >> 1 )
+#define GETBIT02 ( ( flags & 0b0000000000000100 ) >> 2 )
+#define GETBIT03 ( ( flags & 0b0000000000001000 ) >> 3 )
+#define GETBIT04 ( ( flags & 0b0000000000010000 ) >> 4 )
+#define GETBIT05 ( ( flags & 0b0000000000100000 ) >> 5 )
+#define GETBIT06 ( ( flags & 0b0000000001000000 ) >> 6 )
+#define GETBIT07 ( ( flags & 0b0000000010000000 ) >> 7 )
+#define GETBIT08 ( ( flags & 0b0000000100000000 ) >> 8 )
+#define GETBIT09 ( ( flags & 0b0000001000000000 ) >> 9 )
+#define GETBIT10 ( ( flags & 0b0000010000000000 ) >> 10 )
+#define GETBIT11 ( ( flags & 0b0000100000000000 ) >> 11 )
+#define GETBIT12 ( ( flags & 0b0001000000000000 ) >> 12 )
+#define GETBIT13 ( ( flags & 0b0010000000000000 ) >> 13 )
+#define GETBIT14 ( ( flags & 0b0100000000000000 ) >> 14 )
+#define GETBIT15 ( ( flags & 0b1000000000000000 ) >> 15 )
 
-#define SETBIT00 flags |= 0b0000000000000001
-#define SETBIT01 flags |= 0b0000000000000010
-#define SETBIT02 flags |= 0b0000000000000100
-#define SETBIT03 flags |= 0b0000000000001000
-#define SETBIT04 flags |= 0b0000000000010000
-#define SETBIT05 flags |= 0b0000000000100000
-#define SETBIT06 flags |= 0b0000000001000000
-#define SETBIT07 flags |= 0b0000000010000000
-#define SETBIT08 flags |= 0b0000000100000000
-#define SETBIT09 flags |= 0b0000001000000000
-#define SETBIT10 flags |= 0b0000010000000000
-#define SETBIT11 flags |= 0b0000100000000000
-#define SETBIT12 flags |= 0b0001000000000000
-#define SETBIT13 flags |= 0b0010000000000000
-#define SETBIT14 flags |= 0b0100000000000000
-#define SETBIT15 flags |= 0b1000000000000000
+#define SETBIT00 flags |= 0b0000000000000001;
+#define SETBIT01 flags |= 0b0000000000000010;
+#define SETBIT02 flags |= 0b0000000000000100;
+#define SETBIT03 flags |= 0b0000000000001000;
+#define SETBIT04 flags |= 0b0000000000010000;
+#define SETBIT05 flags |= 0b0000000000100000;
+#define SETBIT06 flags |= 0b0000000001000000;
+#define SETBIT07 flags |= 0b0000000010000000;
+#define SETBIT08 flags |= 0b0000000100000000;
+#define SETBIT09 flags |= 0b0000001000000000;
+#define SETBIT10 flags |= 0b0000010000000000;
+#define SETBIT11 flags |= 0b0000100000000000;
+#define SETBIT12 flags |= 0b0001000000000000;
+#define SETBIT13 flags |= 0b0010000000000000;
+#define SETBIT14 flags |= 0b0100000000000000;
+#define SETBIT15 flags |= 0b1000000000000000;
 
 
-#define CLRBIT00 flags &= 0b1111111111111110
-#define CLRBIT01 flags &= 0b1111111111111101
-#define CLRBIT02 flags &= 0b1111111111111011
-#define CLRBIT03 flags &= 0b1111111111110111
-#define CLRBIT04 flags &= 0b1111111111101111
-#define CLRBIT05 flags &= 0b1111111111011111
-#define CLRBIT06 flags &= 0b1111111110111111
-#define CLRBIT07 flags &= 0b1111111101111111
-#define CLRBIT08 flags &= 0b1111111011111111
-#define CLRBIT09 flags &= 0b1111110111111111
-#define CLRBIT10 flags &= 0b1111101111111111
-#define CLRBIT11 flags &= 0b1111011111111111
-#define CLRBIT12 flags &= 0b1110111111111111
-#define CLRBIT13 flags &= 0b1101111111111111
-#define CLRBIT14 flags &= 0b1011111111111111
-#define CLRBIT15 flags &= 0b0111111111111111
+#define CLRBIT00 flags &= 0b1111111111111110;
+#define CLRBIT01 flags &= 0b1111111111111101;
+#define CLRBIT02 flags &= 0b1111111111111011;
+#define CLRBIT03 flags &= 0b1111111111110111;
+#define CLRBIT04 flags &= 0b1111111111101111;
+#define CLRBIT05 flags &= 0b1111111111011111;
+#define CLRBIT06 flags &= 0b1111111110111111;
+#define CLRBIT07 flags &= 0b1111111101111111;
+#define CLRBIT08 flags &= 0b1111111011111111;
+#define CLRBIT09 flags &= 0b1111110111111111;
+#define CLRBIT10 flags &= 0b1111101111111111;
+#define CLRBIT11 flags &= 0b1111011111111111;
+#define CLRBIT12 flags &= 0b1110111111111111;
+#define CLRBIT13 flags &= 0b1101111111111111;
+#define CLRBIT14 flags &= 0b1011111111111111;
+#define CLRBIT15 flags &= 0b0111111111111111;
 
 
 
@@ -135,6 +135,8 @@ uint16_t myuint16 = 0;
 void dosensorread() {
 
   // read sensors into globalcache
+
+  // clear out our globalcache first
   for ( int i = 0 ; i < 16 ; i++ ) {
     globalcache[i] = 0;
   }
@@ -145,73 +147,114 @@ void dosensorread() {
   // use a 16 bit variable as flags for scheduling
   uint16_t flags = 0;
 
-
-
-  if ( GETBIT00 == 0 ) {
-    // fetch the current time. 6 bytes
-    gettimeaddateraw( globalcache );
-    SETBIT00;
-  }
-
-
-  // fetch the current limunance value. 2 bytes
-  // has luminance sensor started?
-  if ( GETBIT01 == 0 ) {
-    // start bh1750 luminance sensor up
-    // set BIT01 to 1;
-  }
-
-  // has luminance sensor been started?
-  if ( GETBIT01 == 1 ) {
-    // has luminance sensor finished?
-    if ( GETBIT02 == 0 ) {
-      // poll for sensor data
-      // if it has arrived, set BIT02 to 1;
+  do {
+      
+    if ( GETBIT00 == 0 ) {
+      // fetch the current time. 6 bytes
+      gettimeaddateraw( globalcache );
+      SETBIT00
     }
-  }
 
 
-  //htu2x
-  // has required order, temperature then humidity
-  if ( GETBIT03 == 0 ) {
-    // start temperature reading
-    // set BIT03 to 1
-  }
-
-  // if temperature reading has begun,
-  if ( GETBIT03 == 1 ) {
-    // has temperature reading finished?
-    if ( GETBIT04 == 0 ) {
-      // poll for data
-      // if it has arrived, set BIT04 to 1;
+    // fetch the current limunance value. 2 bytes
+    // has luminance sensor started?
+    if ( GETBIT01 == 0 ) {
+      // start bh1750 luminance sensor up
+      // set BIT01 to 1;
     }
-  }
 
-  // if temperature reading is done, start humidity reading
-  if ( GETBIT04 == 1 ) {
-    // if I have not send the start command previously
-    if ( GETBIT05 == 0 ) {
-      // send humidity start command
-      // set BIT05 to 1;
+    // has luminance sensor been started?
+    if ( GETBIT01 == 1 ) {
+      // has luminance sensor finished?
+      if ( GETBIT02 == 0 ) {
+        // poll for sensor data
+        // if it has arrived, set BIT02 to 1;
+      }
     }
-  }
 
-  // if humidity start has been sent...
-  if ( GETBIT05 == 1 ) {
-    // is the pressure reading finished?
-    if ( GETBIT06 == 0 ) {
-      // check if any data has arrived
-      // if so, set BIT06 to 1;
+    
+    // fetch the current temperature and humidity.  2x2 bytes
+    //htu2x
+    // has required order, temperature then humidity
+    if ( GETBIT03 == 0 ) {
+      // start temperature reading
+      // set BIT03 to 1
     }
+
+    // if temperature reading has begun,
+    if ( GETBIT03 == 1 ) {
+      // has temperature reading finished?
+      if ( GETBIT04 == 0 ) {
+        // poll for data
+        // if it has arrived, set BIT04 to 1;
+      }
+    }
+
+    // if temperature reading is done, start humidity reading
+    if ( GETBIT04 == 1 ) {
+      // if I have not send the start command previously
+      if ( GETBIT05 == 0 ) {
+        // send humidity start command
+        // set BIT05 to 1;
+      }
+    }
+
+    // if humidity start has been sent...
+    if ( GETBIT05 == 1 ) {
+      // is the pressure reading finished?
+      if ( GETBIT06 == 0 ) {
+        // check if any data has arrived
+        // if so, set BIT06 to 1;
+      }
+    }
+
+
+
+
+    
+    
+    // fetch the current temperature and pressure. 2x2 bytes
+    // bmp085 requires order 1. temperature 2. pressure
+    // has temperature measurement been started.
+    if ( GETBIT07 == 0 ) {
+      // start temperature sensing
+      // set BIT07 to 1
+    }
+
+    // if temperature sensing is not yet completed
+    if ( GETBIT07 == 1 ) {
+      if ( GETBIT08 == 0 ) {
+        // check if any data is available
+        // if so, set BIT08 to 1.
+      }
+    }
+
+    //check if it's time to start pressure sensing
+    if ( GETBIT08 == 1 ) {
+      // check we haven't already sent the command
+      if ( GETBIT09 == 0 ) {
+        // send the pressure start command
+        // set BIT09 to 1.
+      }
+    }
+
+    // if presure start has been sent
+    if ( GETBIT09 == 1 ) {
+      // if results have not yet been received
+      if ( GETBIT10 == 0 ) {
+        // if I have data available
+        // set BIT10 to 1.
+      }
+    }
+
   }
+  while ( ( GETBIT00 == 1 ) && ( GETBIT02 == 1 ) && ( GETBIT06 == 1 ) && ( GETBIT10 == 1 ) );
   
-
-
-
-  
-  // fetch the current temperature and humidity.  2x2 bytes
-
-  // detch the current temperature and pressure. 2x2 bytes
+  // process is complete when the following bits are set.
+  // BIT00 = time retreival is complete.
+  // BIT02 = luminance sensor has finished.
+  // BIT06 = humidity sensor has finished.
+  // BIT10 = pressure sensor has finished.
 
 
 
